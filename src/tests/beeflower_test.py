857a12a70475b30fdc7c36f7975b27cdaade7797
Aino Kruth <aino.kruth@helsinki.fi>
2023-04-18 16:27:14 +0300

    lisä

--- src/tests/beeflower_test.py
+++ src/tests/beeflower_test.py
@@ -1,11 +1,10 @@
 import unittest
 import beeflower
 
+
 class Testbeeflower(unittest.TestCase):
-	def setUp(self):
-		print("Set up goes here")
+    def setUp(self):
+        print("Set up goes here")
 
-	def test_hello_word(self):
-		self.assertEqual("Hello world", "Hello world")
-
-
+    def test_hello_word(self):
+        self.assertEqual("Hello world", "Hello world")
