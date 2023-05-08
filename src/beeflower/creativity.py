d505b4b86d4b78e2b94c4e37506363ccdca65490
Aino Kruth <aino.kruth@helsinki.fi>
2023-05-08 12:06:28 +0300

    lisä

--- src/beeflower/creativity.py
+++ src/beeflower/creativity.py
@@ -1,3 +1,12 @@
+import pygame
+
+class Creativity:
+
+    def __init__(self):
+        self.download_pictures()
+        self.height = 480
+        self.width = 640
+        self.screen = pygame.display.set_mode((self.width, self.height))
     #Downloading the pictures to the game
     def download_pictures(self):
         self.mehilainen = pygame.image.load("mehilainen.png")
@@ -8,41 +17,28 @@
 
     #Building the text:
     def starttext(self):
-        start = False
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    exit()
-                if event.type == event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RETURN:
-                        start = True
-            if start == True:
-                break
-            self.screen.fill((222, 33, 153))
-            pygame.draw.rect(self.screen, (80, 12, 55), (10,10,620,460))
-            pygame.draw.rect(self.screen, (188, 25, 128), (15,15,610,450))
-            text = self.font2.render("Betty Bees honey is all gone,", True, (80, 12, 55))
-            self.screen.blit(text,(self.width / 2 - text.get_width() / 2,100))
-            text = self.font2.render("and she needs more flowers!", True, (80, 12, 55))
-            self.screen.blit(text,(self.width / 2 - text.get_width() / 2,150))
-            text = self.font2.render("Collect Betty 20 flowers,", True, (80, 12, 55))
-            self.screen.blit(text,(self.width / 2 - text.get_width() / 2,200))
-            text = self.font2.render("so she can make a carton of honey again.", True, (80, 12, 55))
-            self.screen.blit(text,(self.width / 2 - text.get_width() / 2,250))
-            text = self.font2.render("Beware of the waterdrops!", True, (80, 12, 55))
-            self.screen.blit(text,(self.width / 2 - text.get_width() / 2,300))
-            text = self.font2.render("You can start the game by pressing enter.", True, (80, 12, 55))
-            self.screen.blit(text,(self.width / 2 - text.get_width() / 2, 350))
-            pygame.display.flip()
+        self.screen.fill((222, 33, 153))
+        pygame.draw.rect(self.screen, (80, 12, 55), (10,10,620,460))
+        pygame.draw.rect(self.screen, (188, 25, 128), (15,15,610,450))
+        text = self.font2.render("Betty Bees honey is all gone,", True, (80, 12, 55))
+        self.screen.blit(text,(self.width / 2 - text.get_width() / 2,100))
+        text = self.font2.render("and she needs more flowers!", True, (80, 12, 55))
+        self.screen.blit(text,(self.width / 2 - text.get_width() / 2,150))
+        text = self.font2.render("Collect Betty 20 flowers,", True, (80, 12, 55))
+        self.screen.blit(text,(self.width / 2 - text.get_width() / 2,200))
+        text = self.font2.render("so she can make a carton of honey again.", True, (80, 12, 55))
+        self.screen.blit(text,(self.width / 2 - text.get_width() / 2,250))
+        text = self.font2.render("Beware of the waterdrops!", True, (80, 12, 55))
+        self.screen.blit(text,(self.width / 2 - text.get_width() / 2,300))
+        text = self.font2.render("You can start the game by pressing enter.", True, (80, 12, 55))
+        self.screen.blit(text,(self.width / 2 - text.get_width() / 2, 350))
+        pygame.display.flip()
 
     #Drawing the screen
     def draw_screen(self):
 
         self.screen.fill((222, 33, 153))
-        if self.left == True:
-            self.screen.blit(self.mehilainencopy, (self.x, self.y))
-        else:
-            self.screen.blit(self.mehilainen, (self.x, self.y))
+        self.screen.blit(self.mehilainen, (self.x, self.y))
         for i in range(self.amounth):
             self.screen.blit(self.kukka, (self.places[i][0], self.places[i][1]))
         for i in range(self.amounth):
@@ -62,22 +58,26 @@
         self.screen.blit(text, (460, self.height-30))
 
         #Defining the screen and the text when the game ends.
-        if self.end_game():
-            #If you win the game:
-            if self.game_passed():
-                pygame.draw.rect(self.screen, (80, 12, 55), (60,155,532,180))
-                pygame.draw.rect(self.screen, (188, 25, 128), (65,160,522,170))
-                text = self.font2.render("Betty is a happy bee with all of her honey!", True, ((80, 12, 55)))
-                self.screen.blit(text,(self.width / 2 - text.get_width()/2 ,223))
-            #And if you lose the game
-            else:
-                pygame.draw.rect(self.screen, (80, 12, 55), (60,145,532,180))
-                pygame.draw.rect(self.screen, (188, 25, 128), (65,150,522,170))
-                text = self.font2.render("Oh no! Betty got wet!!", True, (80, 12, 55))
-                self.screen.blit(text,(self.width / 2 - text.get_width()/2,175))
-                text = self.font3.render(f"High Score: {self.highscore}", True, (80, 12, 55))
-                self.screen.blit(text,(self.width / 2 - text.get_width()/2,220))
-                text = self.font3.render("New Game? Press n.", True, (80, 12, 55))
-                self.screen.blit(text,(self.width / 2 - text.get_width()/2,260))
+        #defining text if game passed
+    def passed(self):
+        pygame.draw.rect(self.screen, (80, 12, 55), (60,155,532,180))
+        pygame.draw.rect(self.screen, (188, 25, 128), (65,160,522,170))
+        text = self.font2.render("Betty is a happy bee with all of her honey!", True, ((80, 12, 55)))
+        self.screen.blit(text,(self.width / 2 - text.get_width()/2 ,223))
+
+        pygame.display.flip()
+        self.clock.tick(60)
+
+        #And if you lose the game
+    def lose(self):
+        pygame.draw.rect(self.screen, (80, 12, 55), (60,145,532,180))
+        pygame.draw.rect(self.screen, (188, 25, 128), (65,150,522,170))
+        text = self.font2.render("Oh no! Betty got wet!!", True, (80, 12, 55))
+        self.screen.blit(text,(self.width / 2 - text.get_width()/2,175))
+        text = self.font3.render(f"High Score: {self.highscore}", True, (80, 12, 55))
+        self.screen.blit(text,(self.width / 2 - text.get_width()/2,220))
+        text = self.font3.render("New Game? Press n.", True, (80, 12, 55))
+        self.screen.blit(text,(self.width / 2 - text.get_width()/2,260))
+
         pygame.display.flip()
         self.clock.tick(60)
--- src/beeflower/funktionality.py
+++ src/beeflower/funktionality.py
@@ -1,6 +1,6 @@
 import pygame
 from random import randint
-
+from creativity import Creativity
 
 class BeeFlower:
 
@@ -8,13 +8,13 @@ class BeeFlower:
     def __init__(self):
 
         pygame.init()
-        self.download_pictures()
+        self.Creativity = Creativity()
 
         self.highscore = 0
         self.height = 480
         self.width = 640
         self.x = 0
-        self.y = self.height-self.mehilainen.get_height()
+        self.y = self.height-self.Creativity.mehilainen.get_height()
 
         self.right = False
         self.left = False
@@ -24,7 +24,7 @@ class BeeFlower:
         self.points = 0
         self.go = True
 
-        self.screen = pygame.display.set_mode((self.width, self.height))
+
         self.font = pygame.font.SysFont("Arial", 26)
         self.font2 = pygame.font.SysFont("Arial", 30)
         self.font3 = pygame.font.SysFont("Arial", 28)
@@ -33,14 +33,26 @@ class BeeFlower:
         pygame.display.set_caption("BeeFlower")
 
         self.go = True
-        self.starttext()
         self.new_game()
         self.loop()
-
+        
+#starting the game
+    def start(self):
+        start = False
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    exit()
+                if event.type == event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        start = True
+            if start == True:
+                break
+            self.Creativity.starttext()
     #Defining the starting points:
     def new_game(self):
         self.x = 0
-        self.y = 480-self.mehilainen.get_height()
+        self.y = 480-self.Creativity.mehilainen.get_height()
         self.amounth = 5
         self.amounth1 = 5
 
@@ -73,7 +85,7 @@ class BeeFlower:
     def loop(self):
         while True:
             self.see_event()
-            self.draw_screen()
+            self.Creativity.draw_screen()
 
     #Defining the events and keys of the game:
     def see_event(self):
@@ -151,19 +163,19 @@ class BeeFlower:
                     self.places[i][1] += 1
 
                 #Defining the starting point of the flower
-                if self.places[i][1]+self.kukka.get_height() >= self.height:
-                    self.places[i][0] = randint(0,self.width-self.kukka.get_width())
+                if self.places[i][1]+self.Creativity.kukka.get_height() >= self.height:
+                    self.places[i][0] = randint(0,self.width-self.Creativity.kukka.get_width())
                     self.places[i][1] = -randint(100,1000)
 
                 #Defining what happens when the bee gets the flower:
-                if self.places[i][1]+self.kukka.get_height()-25 >= self.y:
-                    if self.y+self.mehilainen.get_height()-25 >= self.places[i][1]:
-                        mehilainen_middle = self.x+self.mehilainen.get_width()/2
-                        kukka_middle = self.places[i][0]+self.kukka.get_width()/2
-                        if abs(mehilainen_middle-kukka_middle) <= (self.mehilainen.get_width()+self.kukka.get_width())/2-22:
+                if self.places[i][1]+self.Creativity.kukka.get_height()-25 >= self.y:
+                    if self.y+self.Creativity.mehilainen.get_height()-25 >= self.places[i][1]:
+                        mehilainen_middle = self.x+self.Creativity.mehilainen.get_width()/2
+                        kukka_middle = self.places[i][0]+self.Creativity.kukka.get_width()/2
+                        if abs(mehilainen_middle-kukka_middle) <= (self.Creativity.mehilainen.get_width()+self.Creativity.kukka.get_width())/2-22:
 
                         #Defining a new starting point to the flower
-                            self.places[i][0] = randint(0,self.width-self.kukka.get_width())
+                            self.places[i][0] = randint(0,self.width-self.Creativity.kukka.get_width())
                             self.places[i][1] = -randint(100,1000)
                             self.points += 1
 
@@ -179,16 +191,16 @@ class BeeFlower:
                     self.places1[i][1] += 1
 
                 #Defining the starting point of the waterdrop:
-                if self.places1[i][1]+self.pisara.get_height() >= self.height:
-                    self.places1[i][0] = randint(0,self.width-self.pisara.get_width())
+                if self.places1[i][1]+self.Creativity.pisara.get_height() >= self.height:
+                    self.places1[i][0] = randint(0,self.width-self.Creativity.pisara.get_width())
                     self.places1[i][1] = -randint(100,1000)
 
                 #Defining the game to end when the bee hits a waterdrop:
-                if self.places1[i][1]+self.pisara.get_height()-25 >= self.y:
-                    if self.y+self.mehilainen.get_height()-25  >= self.places1[i][1]:
-                        mehilainen_middle = self.x+self.mehilainen.get_width()/2
-                        pisara_keski = self.places1[i][0]+self.pisara.get_width()/2
-                        if abs(mehilainen_middle-pisara_keski) <= (self.mehilainen.get_width()+self.pisara.get_width())/2-25:
+                if self.places1[i][1]+self.Creativity.pisara.get_height()-25 >= self.y:
+                    if self.y+self.Creativity.mehilainen.get_height()-25  >= self.places1[i][1]:
+                        mehilainen_middle = self.x+self.Creativity.mehilainen.get_width()/2
+                        pisara_keski = self.places1[i][0]+self.Creativity.pisara.get_width()/2
+                        if abs(mehilainen_middle-pisara_keski) <= (self.Creativity.mehilainen.get_width()+self.Creativity.pisara.get_width())/2-25:
                             self.go = False
 
     #Defining highscore and end of the game
@@ -200,6 +212,7 @@ class BeeFlower:
             return True
         else:
             return False
+        self.Creativity.lose()
     #Defining what happens when you win the game
     def game_passed(self):
 
@@ -208,6 +221,7 @@ class BeeFlower:
             return True
         else:
             return False
+        self.Creativity.passed()
 
 if __name__ == "__main__":
     BeeFlower()
