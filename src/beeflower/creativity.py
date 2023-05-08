655828bf94b05e14365d7bd82bb2ecd53f3b4119
Aino Kruth <aino.kruth@helsinki.fi>
2023-05-08 11:12:31 +0300

    lisä

--- src/beeflower/creativity.py
+++ src/beeflower/creativity.py
@@ -5,7 +5,36 @@
         self.mehilainencopy = pygame.transform.flip(self.mehilainen,True, False)
         self.kukka = pygame.image.load("kukka.png")
         self.pisara = pygame.image.load("pisara.png")
-    
+
+    #Building the text:
+    def starttext(self):
+        start = False
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    exit()
+                if event.type == event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        start = True
+            if start == True:
+                break
+            self.screen.fill((222, 33, 153))
+            pygame.draw.rect(self.screen, (80, 12, 55), (10,10,620,460))
+            pygame.draw.rect(self.screen, (188, 25, 128), (15,15,610,450))
+            text = self.font2.render("Betty Bees honey is all gone,", True, (80, 12, 55))
+            self.screen.blit(text,(self.width / 2 - text.get_width() / 2,100))
+            text = self.font2.render("and she needs more flowers!", True, (80, 12, 55))
+            self.screen.blit(text,(self.width / 2 - text.get_width() / 2,150))
+            text = self.font2.render("Collect Betty 20 flowers,", True, (80, 12, 55))
+            self.screen.blit(text,(self.width / 2 - text.get_width() / 2,200))
+            text = self.font2.render("so she can make a carton of honey again.", True, (80, 12, 55))
+            self.screen.blit(text,(self.width / 2 - text.get_width() / 2,250))
+            text = self.font2.render("Beware of the waterdrops!", True, (80, 12, 55))
+            self.screen.blit(text,(self.width / 2 - text.get_width() / 2,300))
+            text = self.font2.render("You can start the game by pressing enter.", True, (80, 12, 55))
+            self.screen.blit(text,(self.width / 2 - text.get_width() / 2, 350))
+            pygame.display.flip()
+
     #Drawing the screen
     def draw_screen(self):
 
--- src/beeflower/funktionality.py
+++ src/beeflower/funktionality.py
@@ -37,36 +37,6 @@ class BeeFlower:
         self.new_game()
         self.loop()
 
-
-    #Building the text:
-    def starttext(self):
-        start = False
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    exit()
-                if event.type == event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RETURN:
-                        start = True
-            if start == True:
-                break
-            self.screen.fill((222, 33, 153))
-            pygame.draw.rect(self.screen, (80, 12, 55), (10,10,620,460))
-            pygame.draw.rect(self.screen, (188, 25, 128), (15,15,610,450))
-            text = self.font2.render("Betty Bees honey is all gone,", True, (80, 12, 55))
-            self.screen.blit(text,(self.width / 2 - text.get_width() / 2,100))
-            text = self.font2.render("and she needs more flowers!", True, (80, 12, 55))
-            self.screen.blit(text,(self.width / 2 - text.get_width() / 2,150))
-            text = self.font2.render("Collect Betty 20 flowers,", True, (80, 12, 55))
-            self.screen.blit(text,(self.width / 2 - text.get_width() / 2,200))
-            text = self.font2.render("so she can make a carton of honey again.", True, (80, 12, 55))
-            self.screen.blit(text,(self.width / 2 - text.get_width() / 2,250))
-            text = self.font2.render("Beware of the waterdrops!", True, (80, 12, 55))
-            self.screen.blit(text,(self.width / 2 - text.get_width() / 2,300))
-            text = self.font2.render("You can start the game by pressing enter.", True, (80, 12, 55))
-            self.screen.blit(text,(self.width / 2 - text.get_width() / 2, 350))
-            pygame.display.flip()
-
     #Defining the starting points:
     def new_game(self):
         self.x = 0
@@ -97,6 +67,7 @@ class BeeFlower:
                         paused = False
             pygame.display.update()
             self.clock.tick(5)
+
 
     #Defining the loop of the game:
     def loop(self):
