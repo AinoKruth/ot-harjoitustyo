4c7ffe8af9ee5975c804a7f9dac130b53b481e6e
Aino Kruth <aino.kruth@helsinki.fi>
2023-05-11 14:56:18 +0300

    lisä

--- src/beeflower/pictures.py
+++ src/beeflower/pictures.py
@@ -3,8 +3,8 @@ import os
 
 class Download_pictures:
     def __init__(self):
-        self.mehilainen = pygame.image.load("../pictures/mehilainen.png")
+        self.mehilainen = pygame.image.load("src/pictures/mehilainen.png")
         self.mehilainencopy = self.mehilainen.copy()
         self.mehilainencopy = pygame.transform.flip(self.mehilainen,True, False)
-        self.kukka = pygame.image.load("../pictures/kukka.png")
-        self.pisara = pygame.image.load("../pictures/pisara.png")
+        self.kukka = pygame.image.load("src/pictures/kukka.png")
+        self.pisara = pygame.image.load("src/pictures/pisara.png")
