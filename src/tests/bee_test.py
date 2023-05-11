2aab0a8932d9d8f052d4494690da3cfe03eadf7a
AinoKruth <128384992+AinoKruth@users.noreply.github.com>
2023-05-11 16:31:14 +0300

    Update bee_test.py

--- src/tests/bee_test.py
+++ src/tests/bee_test.py
@@ -8,7 +8,7 @@ class TestBee(unittest.TestCase):
    def setUp(self):
       self.bee = Bee
       
-   def test_move(self, self.bee.x, self.bee.y):
+   def test_move(self, x, y):
       self.right = True
       maara = self.bee.x
       self.assertEqual(maara, 2)
