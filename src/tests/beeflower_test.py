45e5da96cc6804798a1b809582217e65009d632f
AinoKruth <128384992+AinoKruth@users.noreply.github.com>
2023-05-11 15:48:19 +0300

    Update beeflower_test.py

--- src/tests/beeflower_test.py
+++ src/tests/beeflower_test.py
@@ -1,10 +1,18 @@
 import unittest
-import Bee
+from bee import Bee
 
 
 class TestBee(unittest.TestCase):
+   
    def setUp(self):
-        self.bee = Bee
-        
-   def bee_moves()
+      self.bee = Bee
+      
+   def test_move(self, x,y):
+      self.right = True
+      maara = x
+      self.assertEqual(maara, 2)
+      
+     
+      
+         
 
